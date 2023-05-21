d5f643b4a9fb67230982ae15a21847199009f3cf
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-05-21 12:05:00 +0400

    theme icon ready

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useDispatch, useSelector } from 'react-redux'
 import { setTheme, selecTheme } from '../redux/themeSlice'
 import { selectScreenSize } from '../redux/screenSlice'
+import {faLightbulb,faMoon, faSun} from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
 
@@ -11,21 +13,19 @@ const Header = () => {
 
 console.log(selectedTheme)
     // TEMANI DEYISMEK UCUN FUNKSIYA
-    const ChangeThemeF = (e:any) => {
+    const changeThemeF = () => {
         if(selectedTheme === 'light'){
-            e.target.innerText = 'Light'
             dispatch(setTheme('dark'))
         }
 
         else {
-            e.target.innerText = 'Dark'
             dispatch(setTheme('light'))  
         }
     }
 
 
   return (
-    <div className={selectedTheme === 'dark' ? '  h-20 whitespace-nowrap flex items-center text-sm text-slate-50  hover:text-slate-200 bg-slate-950 shadow-lg ' : ' h-20 whitespace-nowrap flex items-center text-sm text-slate-950 hover:text-slate-800  bg-slate-50 shadow-lg'}>
+    <div className={selectedTheme === 'dark' ? '  h-20 whitespace-nowrap flex items-center text-sm text-slate-50  hover:text-slate-200 bg-slate-950 shadow-lg shadow-slate-400 ' : ' h-20 whitespace-nowrap flex items-center text-sm text-slate-950 hover:text-slate-800  bg-slate-50 shadow-lg shadow-slate-400'}>
         
 <div className='container'>
     <div className='flex justify-between'>
@@ -36,7 +36,8 @@ console.log(selectedTheme)
           <Link className='px-3 py-1 mr-2 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none ' to={'/'}>Fourth Page</Link>
           <Link className='px-3 py -1 mr-2 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none ' to={'/'}>Fifth Page</Link>
         </div>
-<button onClick={ChangeThemeF}>{selectedTheme}</button>
+    <FontAwesomeIcon onClick={changeThemeF} icon={faLightbulb} size ='xl' cursor={'pointer'}  />
+
     </div>
 </div>
 
